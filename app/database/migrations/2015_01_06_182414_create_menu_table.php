15082228526627f2a41096df9fac7b87b28b3838
alexander <alexander.kettl@imendo.at>
2015-01-06 19:41:38 +0100

    added migrations

---
+++ app/database/migrations/2015_01_06_182414_create_menu_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('menus', function($t){
+			$t->increments('id');
+			$t->datetime('menuDate');
+			$t->string('menuTitle');
+			$t->string('menuDescription');
+			$t->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('menus');
+	}
+
+}
