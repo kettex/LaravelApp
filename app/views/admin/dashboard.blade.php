bd6e41ecb99cb709f0dea541fa53c0a4c0c4739f
alexander <alexander.kettl@imendo.at>
2015-01-25 15:25:24 +0100

    deleted statistics for admin/users

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -15,14 +15,6 @@
 @stop
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12 col-sm-12">
-            <div class="panel panel-primary">
-                <div class="panel-heading">Menu Overview</div>
-                <canvas id="menuChartAdmin" height="50px"></canvas>
-            </div>
-        </div>
-    </div>
     <div class="table-responsive">
         <h4 class="margin-bottom-15">Latest ordered Menus</h4>
         <table data-toggle="table"
@@ -42,48 +34,6 @@
             </thead>
         </table>
     </div>
-@stop
-@section('scripts')
-    <script type="text/javascript">
-        // Bar chart
-        var randomScalingFactor = function () {
-            return Math.round(Math.random() * 100)
-        };
-        var barChartData = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    fillColor: "rgba(220,220,220,0.2)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-                },
-                {
-                    label: "My Second dataset",
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-                }
-            ]
-
-        } // lineChartData
-
-        $(document).ready(function () {
-            var ctx_bar = $("#menuChartAdmin")[0].getContext("2d");
-
-            new Chart(ctx_bar).Bar(barChartData, {
-                responsive: true
-            });
-        });
-    </script>
 @stop
 @section('scripts')
     <script type="text/javascript">
